6765813b15f800c27157d9e9e2d26b9bbb075ea3
Gowtham <717823s118@kce.ac.in>
2025-11-11 13:57:17 +0530

    commited by gohut

--- Frontend/src/Pages/Newsbund.jsx
+++ Frontend/src/Pages/Newsbund.jsx
@@ -4,12 +4,26 @@ import { Link } from 'react-router-dom'
 
 export default function Newsbund() {
   return (
-    <div>
-       <nav>
+<div>
+       <nav className='navbar'>
           <div  className='logo'><img src={logo} alt="alt"/></div>     
-          <Link to="/Newsupload"> upload News</Link>    
+          <Link className='link' to="/Newsupload"> upload News</Link>    
        </nav>
-
+    
+    <div className="nb-container">
+      <div className="nb-card">
+      <div className="img-cont"><img src={luffy} alt="" /></div>
+      <div className="textbox">gear5 money d luffy recently rereleased in tam dubed </div>
+      </div>
+       <div className="nb-container2">
+      <div className="nb-card2">
+      <div className="img-cont"><img src={dmk} alt="" /></div>
+      <div className="textbox">Social Media: Political cartoons and art often appear on social media platforms like X (formerly Twitter) and Instagram. News outlets and cartoonists share their work, such as this example from Ananda Vikatan on X and various posts on Instagram using hashtags like #MKStalin or #DMK. </div>
+      </div>
+    </div>
+    
+    </div>
+    
     </div>
   )
 }
